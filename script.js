3d2ef95986d6a486e45f1961c524bf24d915d91b
sirnichemotivation-rgb <sirnichemotivation@gmail.com>
2026-02-19 00:06:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,23 @@ const cloudName = "dntrwkexp";
 let searchTag = eventId || sessionId;
 
 const gallery = document.getElementById('gallery');
+const modal = document.getElementById('photoModal');
+const modalImg = document.getElementById('modalImg');
+const downloadLink = document.getElementById('downloadLink');
+
+// Function para buksan ang picture
+function openPhoto(fullUrl, downloadUrl) {
+    modalImg.src = fullUrl;
+    downloadLink.href = downloadUrl;
+    modal.style.display = "flex";
+}
+
+// Function para isara ang modal
+function closeModal() {
+    modal.style.display = "none";
+}
 
 if (searchTag) {
-    // Kunin ang listahan ng images
     fetch(`https://res.cloudinary.com/${cloudName}/image/list/${searchTag}.json`)
         .then(response => {
             if (!response.ok) throw new Error("No photos found.");
@@ -17,23 +31,20 @@ if (searchTag) {
         .then(data => {
             gallery.innerHTML = ""; 
             
-            // Ayusin: Latest photos sa itaas
             data.resources.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
 
             data.resources.forEach(img => {
-                // Optimized image URL para sa gallery view
                 const thumbUrl = `https://res.cloudinary.com/${cloudName}/image/upload/q_auto,f_auto,w_500/${img.public_id}.${img.format}`;
-                
-                // Full quality URL para sa download
+                const fullViewUrl = `https://res.cloudinary.com/${cloudName}/image/upload/q_auto,f_auto/${img.public_id}.${img.format}`;
                 const downloadUrl = `https://res.cloudinary.com/${cloudName}/image/upload/fl_attachment/${img.public_id}.${img.format}`;
                 
                 const wrapper = document.createElement('div');
                 wrapper.className = "img-card";
                 
-                wrapper.innerHTML = `
-                    <img src="${thumbUrl}" alt="Photo">
-                    <a href="${downloadUrl}" class="download-btn">DOWNLOAD</a>
-                `;
+                // Onclick event para lumaki ang photo
+                wrapper.onclick = () => openPhoto(fullViewUrl, downloadUrl);
+
+                wrapper.innerHTML = `<img src="${thumbUrl}" alt="Photo">`;
                 
                 gallery.appendChild(wrapper);
             });
